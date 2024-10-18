b5f14f72a5719bc219fa8f1254ca34f967f6416a
Anubhav purohit <anubhavpurohitno1@gmail.com>
2024-10-18 21:49:54 +0530

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,8 @@ function App() {
       </div>
       <PdfUpload/>
       {/* <StickyScrollRevealDemo/> */}
-      <MarqueeDemo/>
+      {/* <MarqueeDemo/> */}
+      {/* this is review component */}
       </>
 
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,20 @@ export default {
       boxShadow: {
         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
+      animation: {
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+      },
     },
   },
   plugins: [addVariablesForColors],
